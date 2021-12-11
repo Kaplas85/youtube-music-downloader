33dd2b39e4b999ed3d76598e2b09efa31a09bcb3
Cesar Baudi <cesarbaudi1@gmail.com>
2021-12-11 09:30:14 -0400

    youtube music downloader init

---
+++ main.py
@@ -0,0 +1,30 @@
+# Time
+from datetime import datetime
+
+# PyTube
+from pytube import Playlist
+
+
+def download_playlist(playlist_url: str, folder_name: str) -> None:
+    p: Playlist = Playlist(playlist_url)
+
+    # Create Folder
+    destination = f"./{folder_name}"
+
+    # Downloading
+    for video in p.videos:
+        print(f"Descargando canción {video.title}")
+        video.streams.filter(only_audio=True).first().download(output_path=destination)
+
+
+if __name__ == "__main__":
+
+    playlist_url = str(input("Playlist URL: "))
+    folder_name = str(input("Nombre de la carpeta: "))
+
+    start_time = datetime.now()
+    download_playlist(playlist_url, folder_name)
+
+    print("---------------\n\n")
+    print(f"Playlist descargada en: {start_time - datetime.now()}")
+    print("\n\n---------------")
